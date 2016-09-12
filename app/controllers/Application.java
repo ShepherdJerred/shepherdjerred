577359c3e24e74f309578f0f82544c5fe909be5d
Jerred Shepherd <ShepherdJerred@gmail.com>
2016-09-12 15:32:03 -0500

    Enable forms

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.*;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
@@ -21,6 +23,23 @@ public class Application extends Controller {
 
     public static Result portfolio() {
         return ok(portfolio.render());
+    }
+
+    public static Result sendEmail() {
+
+        MailerClient mailerClient;
+        DynamicForm form = Form.form().bindFromRequest();
+
+        Email email = new Email()
+                .setSubject("Form submission from portfolio site")
+                .setFrom(form.get("email"))
+                .addTo("shepherdjerred@gmail.com")
+                .setBodyText(form.get("message"));
+
+        mailerClient.send(email);
+
+        return ok(index.render());
+
     }
 
 }
