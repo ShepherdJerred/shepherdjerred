1b907ddac6e8b73f467d34b6726d1ce2dda3400b
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-04-28 11:44:59 -0500

    More depends

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const cleanCss = require('gulp-clean-css');
+const concatCss = require('gulp-concat-css');
 
-gulp.task('minifyCss', function () {
-  return gulp.src('./dist/css/*.css')
-    .pipe(cleanCss())
-    .pipe(gulp.dest('./dist/css'));
-});
-
-gulp.task('copy', function () {
-  return gulp.src('./node_modules/normalize.css/normalize.css')
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('bundle', function () {
+  return gulp.src('dist/css/*.css')
+    .pipe(concatCss('dist/css/bundle.css'))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('sass', function () {
@@ -23,4 +18,14 @@ gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'copy', 'minifyCss']);
+gulp.task('normalize', function () {
+  return gulp.src('./node_modules/normalize.css/normalize.css')
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('flexboxgrid', function () {
+  return gulp.src('./node_modules/flexboxgrid/css/flexboxgrid.css')
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['normalize', 'flexboxgrid', 'sass', 'bundle']);
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
+app.get('/', function (req, res) {
+  res.sendfile('./src/html/index.html');
+});
+
 app.listen(port, function () {
   console.log('Site started on port ' + port);
 });
