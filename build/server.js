1d29b08672f65b70e13de9c71a8ddf0772af656d
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-08-20 14:32:40 -0500

    Add Heroku Postbuild to scripts

--- build/server.js
+++ build/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
-  res.sendfile('./src/html/index.html');
+  res.sendfile('./dist/html/index.html');
 });
 
 app.listen(port, function () {
